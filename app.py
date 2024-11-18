653f5428f9cb07d16aee7fc19f431bd7325be49a
juhasz-szabolcs <juhasz.szabolcs@szbi-pg.hu>
2024-11-18 22:04:27 +0100

    Add base structure

---
+++ app.py
@@ -0,0 +1,7 @@
+
+plants = [
+    {"name": "Monstera", "type": "Szobanövény", "feature": "Nagyméretű levelek"},
+    {"name": "Bambusz", "type": "Dísznövény", "feature": "Gyors növekedés"},
+    {"name": "Kaktusz", "type": "Pozsgás", "feature": "Szárazságtűrő"}
+]
+
