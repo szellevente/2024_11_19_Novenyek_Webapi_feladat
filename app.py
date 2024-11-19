afaa369f844d2b093d5d33bcb5a98a870aff9a5c
szellevente <szel.levente@diak.szbi-pg.hu>
2024-11-19 08:49:13 +0100

    update files

--- app.py
+++ app.py
@@ -5,3 +5,19 @@ plants = [
     {"name": "Kaktusz", "type": "Pozsgás", "feature": "Szárazságtűrő"}
 ]
 
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/tabla', methods=['GET'])
+def pizza_order():
+    return render_template('tabla.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
