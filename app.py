5f169895c57dfb76c6ecac6649bb2aa80a418327
szellevente <szel.levente@diak.szbi-pg.hu>
2024-11-19 09:51:50 +0100

    update

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
+from flask import Flask, render_template, request, redirect, url_for
 
 plants = [
     {"name": "Monstera", "type": "Szobanövény", "feature": "Nagyméretű levelek"},
     {"name": "Bambusz", "type": "Dísznövény", "feature": "Gyors növekedés"},
     {"name": "Kaktusz", "type": "Pozsgás", "feature": "Szárazságtűrő"}
 ]
-
-from flask import Flask, render_template, request, redirect, url_for
-
 app = Flask(__name__)
 
 @app.route('/')
